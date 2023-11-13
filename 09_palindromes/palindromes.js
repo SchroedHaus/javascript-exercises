6af9007a5ed0d522ec58db5b4ef4369e95ced46e
SchroedHaus <matt.schroeder@pm.me>
2023-11-13 12:31:02 +0000

    Complete excerise 9 - palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+     const cleanString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+     console.log(cleanString);
 
+     for (let i = 0; i < cleanString.length; i++) {
+        if (cleanString[i] !== cleanString[cleanString.length - 1 - i]) {
+            return false;
+        }
+     }
+     return true;
 };
 
 // Do not edit below this line
