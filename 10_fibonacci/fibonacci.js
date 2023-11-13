8bd12ee931caf4358e56a3cc8b8b3dfb0a359db1
SchroedHaus <matt.schroeder@pm.me>
2023-11-13 13:28:30 +0000

    Complete exercise 10 - fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS";
 
+    const fib = [1, 1];
+
+    for (let i = 2; i < x; i++) {
+        let next = fib[i - 1] + fib[i - 2];
+        fib.push(next);
+    }
+    return fib[x - 1];
 };
 
 // Do not edit below this line
