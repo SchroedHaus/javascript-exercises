273a5f89e709c3b3691c0d108f7e874b1f1e1c19
SchroedHaus <matt.schroeder@pm.me>
2023-11-13 11:52:37 +0000

    Complete exercise 08 - Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return Number(numbers.reduce((acc, current) => acc + current, 0));
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return Number(numbers.reduce((acc, current) => acc * current));
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Number(x ** y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0 || x === 1) return 1;
+
+  for (let i = x - 1; i >= 1; i--) {
+    x *= i;
+  }
+  return x;
 };
 
 // Do not edit below this line
