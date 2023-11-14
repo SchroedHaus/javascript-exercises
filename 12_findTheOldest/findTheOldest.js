ffaad0421c0f628f5642c7760c26e4c6c998c5bd
SchroedHaus <matt.schroeder@pm.me>
2023-11-14 08:51:45 +0000

    Complete exercise 12 - find the oldest.s

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
 
+    people.forEach(person => {
+        let currentYear = new Date().getFullYear();
+        console.log("Current Year: " + currentYear);
+
+        let age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        console.log("Age: " + age);
+
+        if (age > (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth) {
+            oldest = person;
+        }
+
+    })
+    console.log("Oldest: " + oldest.name);
+    return oldest;
 };
 
 // Do not edit below this line
